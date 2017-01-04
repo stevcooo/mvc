95cfbab4a1446eaaf391622c9180297b26c5f744
Stevan Kostoski <stevcooo@gmail.com>
2017-01-04 16:05:04 +0100

    init.

---
+++ samples/ClassesScheduler/ClassesScheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClassesScheduler.Startup))]
+namespace ClassesScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
