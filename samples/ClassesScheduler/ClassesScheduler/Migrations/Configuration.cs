3f44f0a58e85238cb686b17fadd6a4d1ceab25d3
Stevan <stevan@kostoski.com>
2017-01-18 20:24:52 +0100

    Seed profesors and courses.

--- samples/ClassesScheduler/ClassesScheduler/Migrations/Configuration.cs
+++ samples/ClassesScheduler/ClassesScheduler/Migrations/Configuration.cs
@@ -1,31 +1,71 @@
 namespace ClassesScheduler.Migrations
 {
+    using Models;
     using System;
-    using System.Data.Entity;
+    using System.Collections.Generic;    
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ClassesScheduler.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(ClassesScheduler.Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Proffesors.AddOrUpdate(
+                p=>p.FirstName,
+                new Proffesor { FirstName = "Nathan", LastName = "Marshall" },
+                new Proffesor { FirstName = "Homer", LastName = "Munoz" },
+                new Proffesor { FirstName = "Melissa", LastName = "Lindsey" },
+                new Proffesor { FirstName = "Adrian", LastName = "Foster" },
+                new Proffesor { FirstName = "Joyce", LastName = "Hoffman" },
+                new Proffesor { FirstName = "Priscilla", LastName = "Maxwell" },
+                new Proffesor { FirstName = "Lillian", LastName = "Riley" },
+                new Proffesor { FirstName = "Terry", LastName = "Mcgee" },
+                new Proffesor { FirstName = "Jacqueline", LastName = "Poole" },
+                new Proffesor { FirstName = "William", LastName = "Sanchez" },
+                new Proffesor { FirstName = "Dennis", LastName = "Armstrong" },
+                new Proffesor { FirstName = "Sally", LastName = "Hopkins" },
+                new Proffesor { FirstName = "Ian", LastName = "Crawford" },
+                new Proffesor { FirstName = "Rogelio", LastName = "Floyd" },
+                new Proffesor { FirstName = "Adrienne", LastName = "Welch" },
+                new Proffesor { FirstName = "Laura", LastName = "Rodgers" },
+                new Proffesor { FirstName = "Darren", LastName = "Bush" },
+                new Proffesor { FirstName = "Leona", LastName = "Mcguire" },
+                new Proffesor { FirstName = "Monique", LastName = "Roberson" }
+                );
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.SaveChanges();
+
+            int numberOfProffesors = context.Proffesors.Count();
+
+            Random rnd = new Random();
+            Course course;
+            int rndProfId;
+            bool isMandatorySubject;
+            Array semestars = Enum.GetValues(typeof(Enums.Semester));
+
+            Enums.Semester semsterType;
+
+            var courses = new List<Course>();
+
+            //Courses
+            for(int i = 0; i < 80; i++)
+            {
+                rndProfId = rnd.Next(1, numberOfProffesors);
+                isMandatorySubject = (rnd.Next(2) == 0);
+                semsterType = (Enums.Semester)semestars.GetValue(rnd.Next(semestars.Length));
+                course = new Course { Name = String.Format("Course {0}{1}", semsterType, i), Description = "Some description for course.", IsMandatorySubject = isMandatorySubject, NumberOfCredits = rnd.Next(5, 7), SemesterType = semsterType, ProffesorId = context.Proffesors.FirstOrDefault(t => t.Id == rndProfId).Id };
+                courses.Add(course);
+            }
+            
+            context.Courses.AddOrUpdate(
+                p => p.Name,
+                courses.ToArray()                
+                );
         }
     }
 }
